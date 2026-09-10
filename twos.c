e370014a5362aad22334c9fc1143ac086d3e8ca8
csc103 student <me@citymail.ccny.cuny.edu>
2026-09-10 17:40:00 -0400

    tweaks

--- twos.c
+++ twos.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-	int a;
+	unsigned int a;
 	scanf("%d",&a);
 
 	if((a & 0x80000000) >> 31 == 1){
-	printf("decimal: -%d\nbinary: %b\n",((a^0xFFFFFFFF)+1),a);	
+	printf("decimal: -%d\nbinary: %.32b\n",((a^0xFFFFFFFF)+1),a);	
 	} else {
-	printf("decimal: %d\nbinary: %b\n",a,a);
+	printf("decimal: %d\nbinary: %.32b\n",a,a);
 	}
 	return(1);
 
