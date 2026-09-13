868139b9c19e7ea02dae79c6f1acb820e68600ee
csc103 student <me@citymail.ccny.cuny.edu>
2026-09-13 12:37:17 -0400

    fixed display stuff

--- twos.c
+++ twos.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-	unsigned int a;
+	int a;
 	scanf("%d",&a);
 
 	if((a & 0x80000000) >> 31 == 1){
-	printf("decimal: -%d\nbinary: %.32b\n",((a^0xFFFFFFFF)+1),a);	
+	printf("decimal: -%u\nbinary: %.32b\n",((a^0xFFFFFFFF)+1),a);	
 	} else {
-	printf("decimal: %d\nbinary: %.32b\n",a,a);
+	printf("decimal: %u\nbinary: %.32b\n",a,a);
 	}
 	return(1);
 
